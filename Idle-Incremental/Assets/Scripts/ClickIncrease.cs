c0158e27dd40556f9fdb942c73c4688631d74171
Lucas Tsen <tsenlucas@gmail.com>
2018-02-18 20:12:09 +0000

    Added basic click and buttons

---
+++ Idle-Incremental/Assets/Scripts/ClickIncrease.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickIncrease : MonoBehaviour {
+
+	public UnityEngine.UI.Text ScoreDisplay;
+	public int score = 0;
+	public int perClick = 1;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ScoreDisplay.text = "Score: " + score;
+	}
+
+	public void Clicked() {
+		score += perClick;
+
+	}
+
+}
